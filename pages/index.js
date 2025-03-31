4f54c7fad6cdd7231c00dc1f63f67f87231351e3
monurbeser <monurbeser@gmail.com>
2025-03-31 15:03:26 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Features from '@/components/Features';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
+}
