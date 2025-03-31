ac5de3aec6abb62c213d97d307518894144c0f19
monurbeser <monurbeser@gmail.com>
2025-03-31 15:03:06 +0300

    contact.js

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import ContactForm from '@/components/ContactForm';
+
+export default function Contact() {
+  return (
+    <>
+      <Navbar />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
